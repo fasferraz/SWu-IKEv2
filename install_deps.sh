4231510b7625a8d51268feffc8201778333f7429
Dragos Vingarzan <dragos@neatpath.net>
2025-09-28 12:24:35 +0200

    included previous requirements, which I might've missed and improved the README.md

--- install_deps.sh
+++ install_deps.sh
@@ -4,9 +4,22 @@
 set -ueo pipefail
 
 
+<<<<<<< HEAD
 echo -e "\n\n >> Installing dependent libraries...\n\n"
+=======
+echo -e "\n\n >> Installing python3 tools...\n\n"
+sudo apt install -y \
+    python3-pip \
+    python3-setuptools \
+    python3-pyscard
+
+echo -e "\n\n >> Installing depending libraries...\n\n"
+>>>>>>> f88e926 (included previous requirements, which I might've missed and improved the README.md)
 # libpcsclite required by pyscard; the rest are useful for testing with physical smart cards
-sudo apt install -y libpcsclite-dev pcscd pcsc-tools
+sudo apt install -y \
+    libpcsclite-dev \
+    pcscd \
+    pcsc-tools
 
 
 echo -e "\n\n >> Creating and activating a Python virtual environment...\n\n"
